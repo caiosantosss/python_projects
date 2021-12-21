1b6c7d323f50672f689a2f091ed330c37a380db3
Caio Santos <caio.santos@msn.com>
2021-12-21 23:48:46 +0900

    add the questions

--- quiz_game.py
+++ quiz_game.py
@@ -2,3 +2,32 @@ print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play? ")
 print(playing)
+
+if playing != "yes":
+    quit()
+
+print("Ok! Let's play :)")
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer == "graphics processing unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stand for? ")
+if answer == "random access memory":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stand for ")
+if answer == "power supply unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
