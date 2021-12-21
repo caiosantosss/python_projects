cfb5ff9370d2a21ebd09433997ccfcce86ec8c35
Caio Santos <caio.santos@msn.com>
2021-12-21 23:54:18 +0900

    set the score for each correct answer

--- quiz_game.py
+++ quiz_game.py
@@ -7,27 +7,34 @@ if playing.lower() != "yes":
     quit()
 
 print("Ok! Let's play :)")
+score = 0
 
 answer = input("What does CPU stand for? ")
-if answer == "central processing unit":
+if answer.lower() == "central processing unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
 answer = input("What does GPU stand for? ")
-if answer == "graphics processing unit":
+if answer.lower() == "graphics processing unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
 answer = input("What does RAM stand for? ")
-if answer == "random access memory":
+if answer.lower() == "random access memory":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
 answer = input("What does PSU stand for ")
-if answer == "power supply unit":
+if answer.lower() == "power supply unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
+
+print("You got " + score + " questions correct!")
