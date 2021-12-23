6445a01b6cd9a1ec945de07dac434dce73c8c245
Caio Santos <caio.santos@msn.com>
2021-12-23 22:00:20 +0900

    add another path

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -4,8 +4,32 @@ print("Welcome", name, "to this adventure!")
 answer = input("You are on a dirt road, it has come to an end and you can go left or right. Which way would you like to go? ").lower()
 
 if answer == "left":
-    q2 = input("You come to a river, you can walk around it or swim accross? Type walk to walk around and swim to swim accross: ")
+    q2 = input("You come to a river, you can walk around it or swim accross? Type walk to walk around and swim to swim accross: ").lower()
+
+    if q2 == "swim":
+        print("You swam accross and were eaten by an alligator.")
+    elif q2 == "walk":
+        print("You walked fot many miles, ran out of water and you lost the game")
+    else:
+        print("Not a valid option. You lose")
+
 elif answer == "right":
-    print("")
+    q1 == input("You come to a bridge, it looks wobbly, do you want to cross it or head back?(cross/back) ").lower()
+
+    if q2 == "back":
+        print("You go back to the main road. And a car killed you.")
+    elif q2 == "cross":
+        print("You cross the bridge and meet a stranger. Do you want to talk to them?(yes/no) ")
+
+        if q2 == "yes":
+            print("You swam accross and were eaten by an alligator.")
+        elif q2 == "no":
+            print("You walked fot many miles, ran out of water and you lost the game")
+        else:
+            print("Not a valid option. You lose")
+
+    else:
+        print("Not a valid option. You lose")
+
 else:
     print("Not a valid option. You lose")
